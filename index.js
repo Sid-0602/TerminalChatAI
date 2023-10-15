99f38fe55c5ebf220a49e17fdfd71d297905ed69
Siddhant J <sidbnjadhav@gmail.com>
2023-10-15 10:30:58 +0530

    index.js push

---
+++ index.js
@@ -0,0 +1,21 @@
+import { OpenAI } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user', content: 'What is capital of India?' }
+        ],
+    });
+
+    console.log(chatCompletion.choices[0].message.content);
+}
+
+main();
