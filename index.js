6779140ac3df19029c8b94b940b5a44f6a7d3ab5
Siddhant J <sidbnjadhav@gmail.com>
2023-10-15 12:50:59 +0530

    final push

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ async function main() {
     console.log(colors.bold.green("                 << Your Personalized Terminal Chatbot! >>           "));
     console.log(colors.bold.cyan("START CHATTING!"));
 
+    const chatHistory = [];
     //exit when terminated: 
     while(true){
         const userInput = readlineSync.question(colors.yellow('You: '));
 
         try{
             //call the API with user input
-
+            const messages = chatHistory.map(([role,content])=>({role,content})) //construct messages iterating over history.
+            
+            messages.push({role:'user',content:userInput});
+            
             const chatCompletion = await openai.chat.completions.create({
                 model:'gpt-3.5-turbo',
-                messages: [
-                    { 
-                        role: 'user',
-                        content: userInput
-                    }
-                ]
+                messages: messages,
             });
 
             //Get completion text/content from returned response: 
@@ -34,6 +33,10 @@ async function main() {
             }
 
             console.log(colors.green("AI-BOT: ")+ completionText);
+
+            //update history with userInput and response:
+            chatHistory.push(['user',userInput]);
+            chatHistory.push(['assistant',completionText]);
         }catch(error){
             console.error(colors.red(error));
         }
