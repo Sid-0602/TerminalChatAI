b20897929dced65357c663c09de6638048cf29fc
Siddhant J <sidbnjadhav@gmail.com>
2023-10-15 11:37:14 +0530

    user input integrated with readline Sync

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import openai from './config/open-ai.js';
-async function main() {
-    const chatCompletion = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'user', content: 'What is capital of India?' }
-        ],
-    });
+import readlineSync from 'readline-sync';
+import colors from 'colors';
 
-    console.log(chatCompletion.choices[0].message.content);
+async function main() {
+    console.log(colors.bold.green("                 << Welcome to Terminal-Chat-AI >>                 "));
+    console.log(colors.bold.green("                 << Your Personalized Terminal Chatbot! >>           "));
+    console.log(colors.bold.cyan("START CHATTING!"));
+
+    //exit when terminated: 
+    while(true){
+        const userInput = readlineSync.question(colors.yellow('You: '));
+
+        try{
+            //call the API with user input
+
+            const chatCompletion = await openai.chat.completions.create({
+                model:'gpt-3.5-turbo',
+                messages: [
+                    { 
+                        role: 'user',
+                        content: userInput
+                    }
+                ]
+            });
+
+            //Get completion text/content from returned response: 
+            const completionText = chatCompletion.choices[0].message.content;
+
+            //exit condition: 
+            if(userInput.toLowerCase()=='_exit_'){
+                console.log(colors.green("AI-BOT: ")+ completionText);
+                return;
+            }
+
+            console.log(colors.green("AI-BOT: ")+ completionText);
+        }catch(error){
+            console.error(colors.red(error));
+        }
+    } 
 }
 
 main();
